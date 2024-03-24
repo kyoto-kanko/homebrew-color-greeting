804dc1f09a123d1ade898c481e9b4c73fa2b51c8
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-24 14:38:32 +0900

    sha256更新

--- color_greeting.rb
+++ color_greeting.rb
@@ -1,8 +1,8 @@
 class ColorGreeting < Formula
     desc "Simple Ruby CLI tool"
     homepage "https://github.com/kyoto-kanko/color-greeting"
-    url "https://github.com/kyoto-kanko/color-greeting/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "b9732f18e232b77dfd6f72fc68505591df89f4d8fdbd399fe1c9f11489e0536b"
+    url "https://github.com/kyoto-kanko/color-greeting/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "fed86958a043b9d2a3d2ac6e2bcef78ccaacf759ffddf6d59ede33464ec42b06"
     license "MIT"
   
     depends_on "ruby"
