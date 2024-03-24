c26283a055bb986b64722ef10a232f22875cf60d
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-24 16:19:04 +0900

    完成

--- color_greeting.rb
+++ color_greeting.rb
@@ -1,32 +1,29 @@
 class ColorGreeting < Formula
-  desc "Simple Ruby CLI tool that greets you"
-  homepage "https://github.com/kyoto-kanko/color-greeting"
-  url "https://github.com/kyoto-kanko/color-greeting/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "fed86958a043b9d2a3d2ac6e2bcef78ccaacf759ffddf6d59ede33464ec42b06"
-  license "MIT"
-
-  depends_on "ruby"
-
-  resource "colorize" do
-    url "https://rubygems.org/downloads/colorize-1.1.0.gem"
-    sha256 "30b5237f0603f6662ab8d1fc2bd4a96142b806c6415d79e45ef5fdc6a0cfc837"
-  end
-
-  def install
-    ENV["GEM_HOME"] = libexec
+    desc "シンプルなRubyのCLIツールで挨拶をします"
+    homepage "https://github.com/kyoto-kanko/color-greeting"
+    url "https://github.com/kyoto-kanko/color-greeting/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "fed86958a043b9d2a3d2ac6e2bcef78ccaacf759ffddf6d59ede33464ec42b06"
+    license "MIT"
   
-    resources.each do |r|
-      r.fetch
-      system "gem", "install", r.cached_download, "--ignore-dependencies",
-             "--no-document", "--install-dir", libexec
+    depends_on "ruby"
+  
+    resource "colorize" do
+      url "https://rubygems.org/downloads/colorize-1.1.0.gem"
+      sha256 "30b5237f0603f6662ab8d1fc2bd4a96142b806c6415d79e45ef5fdc6a0cfc837"
     end
   
-    # Instead of bin.install libexec/"bin/hello"
-    # Use the line below to install the 'hello' script directly from your project's bin directory
-    bin.install "bin/hello"
+    def install
+      ENV["GEM_HOME"] = libexec
   
-    # This sets up the environment so that when the 'hello' script is run, it knows where to find its Ruby gems.
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+      resources.each do |r|
+        r.fetch
+        system "gem", "install", r.cached_download, "--ignore-dependencies",
+               "--no-document", "--install-dir", libexec
+      end
+  
+      bin.install "bin/hello"
+  
+      bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    end
   end
   
-end
